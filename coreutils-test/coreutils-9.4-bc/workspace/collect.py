1e009264b4682f64806ddddbe1a0448144b82c3c
xuruiyang2002 <xury5520@outlook.com>
2024-10-06 20:45:29 +0800

    Update script

--- coreutils-test/coreutils-9.4-bc/workspace/collect.py
+++ coreutils-test/coreutils-9.4-bc/workspace/collect.py
@@ -1,18 +1,15 @@
 import subprocess
 import csv
-
 program_list = """ base64 basename cat chcon chgrp chmod chown chroot comm cp csplit cut
-date dd df dirname du echo env expand expr factor false fmt fold ginstall head hostid 
+date dd df dirname du echo env expand expr factor false fmt fold ginstall head hostid
 id join kill link ln logname ls md5sum mkdir mkfifo mknod mktemp mv nice nl nohup od paste pathchk
 pinky pr printenv printf ptx pwd readlink rm rmdir runcon seq shuf split stat stty
 sum sync tac tail tee touch tr tsort tty uname unexpand uniq unlink uptime users who whoami pwd tr
 """.split()
-
 output_dict = {}
-
 for program in program_list:
     print(f"====> {program}")
-    command = f"./collect.sh {program}"
+    command = f"./collect_cov.sh {program}"
     try:
         result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         output_lines = result.stdout.decode('utf-8').split('\n')
@@ -22,24 +19,13 @@ for program in program_list:
         output_dict[program] = ["Error occurred", str(e)]
     except Exception as e:
         output_dict[program] = ["Exception occurred", str(e)]
-
 # Write the collected data to a CSV file
 with open('program_output.csv', 'w', newline='') as csvfile:
     fieldnames = ['program', 'line_cov', 'branch_cov']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
     writer.writeheader()
     for program, lines in output_dict.items():
         # Ensure there are at least three elements in lines
         while len(lines) < 3:
             lines.append("")
         writer.writerow({'program': program, 'line_cov': lines[0], 'branch_cov': lines[1] if len(lines) > 1 else ''})
-
-
-# Display the output
-for program, lines in output_dict.items():
-    print(f"{program} output:")
-    for line in lines:
-        print(line)
-    print("\n" + "-"*40 + "\n")
-
--- coreutils-test/coreutils-9.4-bc/workspace/collect.sh
+++ coreutils-test/coreutils-9.4-bc/workspace/collect.sh
@@ -1,7 +1,20 @@
 cd /home/user/coreutils-test/coreutils-9.4-src/src
 
-/home/user/ifse/build/bin/klee-replay /home/user/coreutils-test/coreutils-9.4-src/src/$1 /home/user/coreutils-data/result_all/$1-120min-ifse/*.ktest
+# IFSE
+/home/user/recolossus/build/bin/klee-replay /home/user/coreutils-test/coreutils-9.4-src/src/$1 /home/user/coreutils-test/coreutils-9.4-bc/workspace/result_all/$1-240min-original/*.ktest
+# KLEE
+# /home/user/recolossus/build/bin/klee-replay /home/user/coreutils-test/coreutils-9.4-src/src/$1 /home/user/coreutils-test/coreutils-9.4-bc/workspace/result_all/$1-240min-colossus/*.ktest
 
-gcov -b $1.c | head -3
+if [ $1 = "base64" ]; then
+    gcov -b base64-basenc | head -3
+elif [ $1 = "ginstall" ]; then
+    gcov -b install | head -3
+elif [ $1 = "md5sum" ]; then
+    gcov -b md5sum-digest | head -3
+elif [ $1 = "sum" ]; then
+    gcov -b sum-digest | head -3
+else
+    gcov -b $1.c | head -3
+fi
 
 rm *.gcda
